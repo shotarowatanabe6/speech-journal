aa12f7ceae3bb6b172a72913328e6e76720b00fb
shotarowatanabe6 <shotarowatanabe6@gmail.com>
2026-04-25 14:40:58 +0900

    feat: shadcn/uiを導入

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import "./App.css";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
       <section id="center">
         <div>
           <p class="text-3xl font-bold underline">Hello world!</p>
+          <Button variant={"outline"}>shadcn/uiで作成したボタン</Button>
         </div>
       </section>
     </>
